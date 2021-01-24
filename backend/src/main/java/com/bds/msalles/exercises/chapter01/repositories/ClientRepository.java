1e5cdf8f2d4d878b3591c2808655850d4663c543
Mario Salles <mariofsalles@gmail.com>
2021-01-24 22:53:39 +0000

    H2 database

---
+++ backend/src/main/java/com/bds/msalles/exercises/chapter01/repositories/ClientRepository.java
@@ -0,0 +1,11 @@
+package com.bds.msalles.exercises.chapter01.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.bds.msalles.exercises.chapter01.entities.Client;
+
+@Repository
+public interface ClientRepository extends JpaRepository<Client, Long> {
+
+}
