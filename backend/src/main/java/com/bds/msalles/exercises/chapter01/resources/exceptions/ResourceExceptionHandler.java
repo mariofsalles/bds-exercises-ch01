2da10e091694c0c0b889fdc1eee3777d3bee0c38
Mario Salles <mariofsalles@gmail.com>
2021-01-25 22:05:51 +0000

    UPDATE and DELETE /clients/{:id}

--- backend/src/main/java/com/bds/msalles/exercises/chapter01/resources/exceptions/ResourceExceptionHandler.java
+++ backend/src/main/java/com/bds/msalles/exercises/chapter01/resources/exceptions/ResourceExceptionHandler.java
@@ -9,19 +9,34 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
-import com.bds.msalles.exercises.chapter01.exceptions.EntityNotFoundException;
+import com.bds.msalles.exercises.chapter01.exceptions.DatabaseException;
+import com.bds.msalles.exercises.chapter01.exceptions.ResourceNotFoundException;
 
 @ControllerAdvice
 public class ResourceExceptionHandler {
 	
-	@ExceptionHandler(EntityNotFoundException.class)
-	public ResponseEntity<StandardError> entityNotFound(EntityNotFoundException e, HttpServletRequest request){
+	@ExceptionHandler(ResourceNotFoundException.class)
+	public ResponseEntity<StandardError> entityNotFound(ResourceNotFoundException e, HttpServletRequest request){
+		HttpStatus status = HttpStatus.NOT_FOUND;
 		StandardError err = new StandardError();
 		err.setTimestamp(Instant.now());
-		err.setStatus(HttpStatus.NOT_FOUND.value());
+		err.setStatus(status.value());
 		err.setError("Resource not found");
 		err.setMessage(e.getMessage());
 		err.setPath(request.getRequestURI());
 		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);
 	}
+	
+	@ExceptionHandler(DatabaseException.class)
+	public ResponseEntity<StandardError> database(DatabaseException e, HttpServletRequest request){
+	    HttpStatus status = HttpStatus.BAD_REQUEST;
+	    StandardError err = new StandardError();
+	    err.setTimestamp(Instant.now());
+	    err.setStatus(status.value());
+	    err.setError("Database exception");
+	    err.setMessage(e.getMessage());
+	    err.setPath(request.getRequestURI());
+	    return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
+	}
+	
 }
--- backend/src/main/java/com/bds/msalles/exercises/chapter01/services/ClientService.java
+++ backend/src/main/java/com/bds/msalles/exercises/chapter01/services/ClientService.java
@@ -4,13 +4,18 @@ import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.bds.msalles.exercises.chapter01.dto.ClientDTO;
 import com.bds.msalles.exercises.chapter01.entities.Client;
-import com.bds.msalles.exercises.chapter01.exceptions.EntityNotFoundException;
+import com.bds.msalles.exercises.chapter01.exceptions.DatabaseException;
+import com.bds.msalles.exercises.chapter01.exceptions.ResourceNotFoundException;
 import com.bds.msalles.exercises.chapter01.repositories.ClientRepository;
 
 @Service
@@ -28,7 +33,7 @@ public class ClientService {
 	@Transactional(readOnly = true)
 	public ClientDTO findById(Long id) {
 		Optional<Client> obj = repository.findById(id);
-		Client entity = obj.orElseThrow(()->new EntityNotFoundException("Entity not found"));
+		Client entity = obj.orElseThrow(()-> new ResourceNotFoundException("Entity not found"));
 		return new ClientDTO(entity);
 	}
 	
@@ -43,5 +48,34 @@ public class ClientService {
 		entity = repository.save(entity);
 		return new ClientDTO(entity);
 	}
-	
+
+	@Transactional
+	public ClientDTO update(Long id, ClientDTO dto) {
+		try {
+			Client entity = repository.getOne(id);
+			entity.setName(dto.getName());
+			entity.setCpf(dto.getCpf());
+			entity.setIncome(dto.getIncome());
+			entity.setBirthDate(dto.getBirthDate());
+			entity.setChildren(dto.getChildren());
+			entity = repository.save(entity);
+			return new ClientDTO(entity);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException("Id not found: " + id);
+		}
+	}
+
+	public void destroy(Long id) {
+		try {
+			repository.deleteById(id);			
+		} 
+		catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException("Id not found: " + id);
+		}
+		catch (DataIntegrityViolationException e) {
+		    throw new DatabaseException("Integrity violation");
+		}
+	}
+
+
 }
